454e0988a00d15c5af42f960a69b7519a99a327f
denisbrkv <buryakovdv@gmail.com>
2025-02-20 01:08:12 +0400

    add matine test

---
+++ src/widgets/footer/footer.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Anchor, Container, Group } from "@mantine/core";
+import { MantineLogo } from "@mantinex/mantine-logo";
+import styles from "./styles.module.css";
+
+const links = [
+  { link: "#", label: "Contact" },
+  { link: "#", label: "Privacy" },
+  { link: "#", label: "Blog" },
+  { link: "#", label: "Careers" },
+];
+
+export const Footer = () => {
+  const items = links.map((link) => (
+    <Anchor<"a">
+      c="dimmed"
+      key={link.label}
+      href={link.link}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <footer className={styles.footer}>
+      <Container className={styles.inner}>
+        <MantineLogo size={28} />
+        <Group className={styles.links}>{items}</Group>
+      </Container>
+    </footer>
+  );
+};
