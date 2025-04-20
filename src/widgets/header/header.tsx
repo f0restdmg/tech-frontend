3f14bed84dee999e60bded89cda45744b772c92a
denisbrkv <buryakovdv@gmail.com>
2025-04-20 20:19:48 +0400

    optimize header

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,55 +1,17 @@
-"use client";
-
-import Link from "next/link";
-import { useState } from "react";
 import clsx from "clsx";
-import { mainMenu } from "@/widgets/header/navbar/config";
 import { Logo } from "@/shared/ui/logo";
+import { MobileBurger } from "./mobile-burger";
 import { Navbar } from "./navbar";
 import styles from "./header.module.css";
 
 export const Header = () => {
-  const [isDrawerOpen, setDrawerOpen] = useState(false);
-
-  const toggleDrawer = () => {
-    setDrawerOpen(!isDrawerOpen);
-  };
-
   return (
     <header className={clsx(styles.header)}>
       <div className={clsx(styles.header__inner, "container")}>
         <Logo className={styles.header__logo} />
         <Navbar />
         <div className={styles.header__actions}></div>
-        <button
-          className={`${styles.header__burger} ${isDrawerOpen ? styles["header__burger--open"] : ""}`}
-          aria-label="Открыть меню"
-          onClick={toggleDrawer}
-        >
-          <span className={styles.header__line}></span>
-          <span className={styles.header__line}></span>
-          <span className={styles.header__line}></span>
-        </button>
-        <div
-          className={`${styles.header__drawer} ${isDrawerOpen ? styles["header__drawer--open"] : ""}`}
-        >
-          <div className={styles.header__mobile} onClick={toggleDrawer}>
-            <div className={styles["styles.header__actions--mobile"]}></div>
-            <ul className={styles["header__mobile-menu"]}>
-              {mainMenu.map((item, index) => (
-                <li key={index}>
-                  <Link
-                    className={styles["header__mobile-menu--link"]}
-                    href={item.link}
-                    prefetch={false}
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+        <MobileBurger />
       </div>
     </header>
   );
---
+++ src/widgets/header/mobile-burger/index.ts
@@ -0,0 +1,1 @@
+export { MobileBurger } from "./mobile-burger";
