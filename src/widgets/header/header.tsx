161b4af2ab35fc906dad24d6665a1c6d6ebeca13
denisbrkv <buryakovdv@gmail.com>
2025-04-20 22:05:55 +0400

    fix language picker

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,9 +1,9 @@
 import clsx from "clsx";
 import { Logo } from "@/shared/ui/logo";
+import { LanguagePicker } from "@/shared/ui/language-picker";
 import { MobileBurger } from "./mobile-burger";
 import { Navbar } from "./navbar";
 import styles from "./header.module.css";
-import { LanguagePicker } from "@/shared/ui/language-picker";
 
 export const Header = () => {
   return (
