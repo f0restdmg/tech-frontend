5a0dada9418585483359bd62b72aa6b3fe772299
denisbrkv <buryakovdv@gmail.com>
2025-04-21 01:36:25 +0400

    refactor main

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,26 +1,5 @@
-import { useTranslations } from "next-intl";
+import { Main } from "@/pages/main";
 
 export default function Home() {
-  const t = useTranslations("Home");
-
-  return (
-    <div className={"container"}>
-      <h1>{t("title")}</h1>
-      <ul>
-        <li>
-          h1 - Блок выбора сравнений видеокарт, процессоров как у -
-          https://technical.city/en
-        </li>
-        <li>
-          Опционально - Только с дизайном, рекламный блок топ распродаж как у -
-          https://cpu.userbenchmark.com/
-        </li>
-        <li>Топ 10 процессоров новых последних выпущенных</li>
-        <li>Топ 10 процессоров по просмотрам</li>
-        <li>Топ 10 видеокарт новых последних выпущенных</li>
-        <li>Топ 10 видеокарт по просмотрам</li>
-        <li>Топ 10 стравнений по популярности</li>
-      </ul>
-    </div>
-  );
+  return <Main />;
 }
