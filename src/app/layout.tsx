6c5031e57ef982e27b7c2fd05f7184628f59a132
DenisBrkv <buryakovdv@gmail.com>
2025-10-15 13:11:31 +0400

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { manrope } from "@/shared/config/fonts";
 
 export const metadata: Metadata = {
   title: "HardwareBench",
@@ -22,10 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="ru">
+      <body className={manrope.variable}>{children}</body>
     </html>
   );
 }
---
+++ src/shared/config/fonts.ts
@@ -0,0 +1,18 @@
+import localFont from "next/font/local";
+
+export const manrope = localFont({
+  src: [
+    {
+      path: "./assets/manrope-v20-latin-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./assets/manrope-v20-latin-600.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-family-main",
+  display: "swap",
+});
